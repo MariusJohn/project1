125987a8f9b37587f614a1ea1f5cc5cc7f9aa5b7
MariusJohn <marius.ciuclea@gmail.com>
2022-11-29 21:24:59 +0000

    added script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let randonNumber = Math.floor(Math.random() * 100) + 1;
+
+const   guesses = document.querySelector('.guesses');
+const   lastResult = document.querySelector('.lastResult');
+const   lowOrHi = document.querySelector('.lowOrhi');
+const   guessSubmit = document.querySelector('.guessSubmut');
+const    guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
